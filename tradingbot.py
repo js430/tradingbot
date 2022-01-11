3a75e51ebadd393e6b35c407bcc76f691998c7bf
Jeffrey Shi <jeffreyshi430@gmail.com>
2022-01-11 14:57:19 -0500

    v1.1.6:added weekly recap

--- tradingbot.py
+++ tradingbot.py
@@ -4,7 +4,7 @@ import discord
 import random
 import time
 import string
-from datetime import date
+from datetime import date, datetime, timedelta
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.utils import get
@@ -14,12 +14,16 @@ load_dotenv()
 TOKEN=os.getenv('DISCORD_TOKEN')
 GUILD=os.getenv('DISCORD_GUILD')
 
+TEST=False
 server_everyone=[788637809963302922,778326013998006284, 727350310384959508]
 # x, x, Ace of trades
-alert_channels=[928360625632067664, 928741202776457257, 928345916128247868, 927611713879146546, 925526435500793926, 929520109788209152,930267744341983244]
-#alert_channels=[928741202776457257, 929520109788209152]
-#Stock Degen, bot test, stock lounge, crimson, freedom, Ace of trades
+if TEST:
+    alert_channels=[928741202776457257]
+else:
+    alert_channels=[928360625632067664, 928741202776457257, 928345916128247868, 927611713879146546, 925526435500793926, 929520109788209152,930267744341983244]
 #bot test
+#Stock Degen, bot test, stock lounge, crimson, freedom, Ace of trades
+
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -126,16 +130,21 @@ async def recap(ctx, tickers, percents):
     for i in percs:
         if i[0]=='+':
             rg.append('g')
-        else:
+        elif i[0]=='-':
             rg.append('r')
+        else:
+            rg.append('n')
     embed_string=""
     for i in range(0, len(ticks)):
         #print(i)
         result=""
-        result=result+ticks[i]+" "+percs[i]+"% "
+        if(rg[i]=='n'):
+            result=result+ticks[i]+":"+" B/E"
+        else:
+            result=result+ticks[i]+": "+percs[i]+"% "
         if(rg[i]=='g'):
             result=result+" <:green_circle:930207873961697412>"
-        else:
+        elif(rg[i]=='r'):
             result=result+" <:red_circle:930208152559956028>"
         result=result+"\n"
         #print(result)
@@ -144,18 +153,87 @@ async def recap(ctx, tickers, percents):
     for i in range(0, len(percs)):
         if rg[i]=='g':
             total=total+int(percs[i][1:])
-        else:
+        elif rg[i]=='r':
             total=total-int(percs[i][1:])
     embed_string=embed_string+"\n \n Total:"+str(total)
     if(total>100):
         embed_string=embed_string+"%<:rocket:930210721655046144>\n"
     avg_gain=round(total/len(percs),2)
-    Winrate=round(rg.count('g')/len(rg)*100,2)
+    Winrate=round((rg.count('g')+rg.count('n'))/len(rg)*100,2)
     embed_string=embed_string+"\nWinrate="+str(Winrate)+"%\n"+"AvgGain="+str(avg_gain)+ "%"+" per trade"
 
     today=date.today()
     today_date = today.strftime("%m/%d")
-    embed=discord.Embed(title= today_date+" recap", description=embed_string)
+    if(total>0):
+        color=0x00FF00
+    else:
+        color=0xFF5733
+    embed=discord.Embed(title= today_date+" recap", description=embed_string, color=color)
+    for guilds in bot.guilds:
+        if guilds.id in server_everyone:
+            for channel in guilds.channels:
+                if(channel.id in alert_channels):
+                    await channel.send(ctx.message.guild.default_role, embed=embed)
+        else:
+            if(guilds.id==911385966864896081):
+                role=get(guilds.roles, id=911690821739356170)
+            else:
+                role=get(guilds.roles, name=('prime-alerts'))
+            for channel in guilds.channels:
+                if(channel.id in alert_channels):
+                #if(channel.name == 'test-channel'):
+                    await channel.send(role.mention, embed=embed)
+
+@bot.command(name='wrecap')
+@commands.has_role('Prime')
+async def recap(ctx, tickers, percents):
+    tickers=str(tickers)
+    ticks=tickers.split(',')
+    percents=str(percents)
+    percs=percents.split(',')
+    rg=[]
+    for i in percs:
+        if i[0]=='+':
+            rg.append('g')
+        elif i[0]=='-':
+            rg.append('r')
+        else:
+            rg.append('n')
+    embed_string=""
+    for i in range(0, len(ticks)):
+        #print(i)
+        result=""
+        if(rg[i]=='n'):
+            result=result+ticks[i]+": "+"B/E"
+        else:
+            result=result+ticks[i]+": "+percs[i]+"% "
+        if(rg[i]=='g'):
+            result=result+" <:green_circle:930207873961697412>"
+        elif(rg[i]=='r'):
+            result=result+" <:red_circle:930208152559956028>"
+        result=result+"\n"
+        #print(result)
+        embed_string=embed_string+result
+    total=0
+    for i in range(0, len(percs)):
+        if rg[i]=='g':
+            total=total+int(percs[i][1:])
+        elif rg[i]=='r':
+            total=total-int(percs[i][1:])
+    embed_string=embed_string+"\n \n Total:"+str(total)
+    if(total>100):
+        embed_string=embed_string+"%<:rocket:930210721655046144>\n"
+    avg_gain=round(total/len(percs),2)
+    Winrate=round((rg.count('g')+rg.count('n'))/len(rg)*100,2)
+    embed_string=embed_string+"\nWinrate="+str(Winrate)+"%\n"+"AvgGain="+str(avg_gain)+ "%"+" per trade"
+
+    today=datetime.now()
+    monday=(today-timedelta(days=today.weekday())).strftime("%m/%d")
+    if(total>0):
+        color=0x00FF00
+    else:
+        color=0xFF5733
+    embed=discord.Embed(title= "Week of "+monday+" recap", description=embed_string, color=color)
     for guilds in bot.guilds:
         if guilds.id in server_everyone:
             for channel in guilds.channels:
